0c824df116bbc3c36997b55c7ff912db68e15164
DESKTOP-L3QTLOV\User <verto5828@gmail.com>
2024-07-24 23:46:30 +0800

    Updated

--- download_images.py
+++ download_images.py
@@ -11,7 +11,7 @@ driver = webdriver.Chrome()
 driver.get("https://www.pinterest.com/")
 
 login_button = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, '//*[@id="mweb-unauth-container"]/div/div/div[1]/div/div[2]/div[2]/button/div/div'))
+    EC.presence_of_element_located((By.XPATH, '//*[@id="__PWS_ROOT__"]/div/div[1]/div/div[1]/div/div[2]/div[2]/button/div/div'))
 )
 login_button.click()
 time.sleep(3)
@@ -26,32 +26,35 @@ login = driver.find_element(By.XPATH, '//*[@id="__PWS_ROOT__"]/div/div[1]/div[2]
 
 username.clear()
 password.clear()
-username.send_keys('你的帳號')
-password.send_keys('你的密碼')
+username.send_keys('your username')
+password.send_keys('your password')
 login.click()
 
 search = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, '//*[@id="searchBoxContainer"]/div/div/div[2]/input'))
+    EC.presence_of_element_located((By.NAME, 'searchBoxInput'))
 )
-keyword = "關鍵字"
+search.clear()
+keyword = "dogs"
 search.send_keys(keyword)
 search.send_keys(Keys.RETURN) 
 
+# images loading
 WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.CLASS_NAME, "hCL.kVc.L4E.MIw"))
 )
 
+# create a folder to save images
 count = 1
 path = os.path.join(keyword)
 os.mkdir(path) 
  
-for i in range(5):  
+for i in range(3):  
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") 
-    time.sleep(5)  
+    time.sleep(5)  # for loading images
     
     imgs = driver.find_elements(By.CLASS_NAME, "hCL.kVc.L4E.MIw")
     
     for img in imgs:
         save_as = os.path.join(path, keyword + str(count) + '.jpg') 
-        wget.download(img.get_attribute("src"), save_as) 
+        wget.download(img.get_attribute("src"), save_as)  # download images
         count += 1
